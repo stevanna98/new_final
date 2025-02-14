cd9d6ea45461a34eb8853d05a364c19c22a5b9ad
Stefano Vannoni <s.vannoni98.lin@gmail.com>
2025-02-14 09:49:47 +0100

    fixed

--- src/l0_regularizer.py
+++ src/l0_regularizer.py
@@ -2,6 +2,7 @@ import torch
 import math
 import torch.nn.functional as F
 from torch.nn.modules import Module
+from lightning.pytorch import pl
 from torch.nn.parameter import Parameter
 from torch.nn.modules.utils import _pair as pair
 from torch.autograd import Variable
@@ -9,21 +10,9 @@ from torch.nn import init
 
 limit_a, limit_b, epsilon = -.1, 1.1, 1e-6
 
-
-class L0Linear(Module):
-    """Implementation of L0 regularization for the input units of a fully connected layer"""
+class L0Linear(pl.LightningModule):
     def __init__(self, in_features, out_features, bias=True, weight_decay=1., droprate_init=0.5, temperature=2./3.,
                  lamba=1., local_rep=False, **kwargs):
-        """
-        :param in_features: Input dimensionality
-        :param out_features: Output dimensionality
-        :param bias: Whether we use a bias
-        :param weight_decay: Strength of the L2 penalty
-        :param droprate_init: Dropout rate that the L0 gates will be initialized to
-        :param temperature: Temperature of the concrete distribution
-        :param lamba: Strength of the L0 penalty
-        :param local_rep: Whether we will use a separate gate sample per element in the minibatch
-        """
         super(L0Linear, self).__init__()
         self.in_features = in_features
         self.out_features = out_features
@@ -54,18 +43,15 @@ class L0Linear(Module):
         self.qz_loga.data.clamp_(min=math.log(1e-2), max=math.log(1e2))
 
     def cdf_qz(self, x):
-        """Implements the CDF of the 'stretched' concrete distribution"""
         xn = (x - limit_a) / (limit_b - limit_a)
         logits = math.log(xn) - math.log(1 - xn)
         return F.sigmoid(logits * self.temperature - self.qz_loga).clamp(min=epsilon, max=1 - epsilon)
 
     def quantile_concrete(self, x):
-        """Implements the quantile, aka inverse CDF, of the 'stretched' concrete distribution"""
         y = F.sigmoid((torch.log(x) - torch.log(1 - x) + self.qz_loga) / self.temperature)
         return y * (limit_b - limit_a) + limit_a
 
     def _reg_w(self):
-        """Expected L0 norm under the stochastic gates, takes into account and re-weights also a potential L2 penalty"""
         logpw_col = torch.sum(- (.5 * self.prior_prec * self.weights.pow(2)) - self.lamba, 1)
         logpw = torch.sum((1 - self.cdf_qz(0)) * logpw_col)
         logpb = 0 if not self.use_bias else - torch.sum(.5 * self.prior_prec * self.bias.pow(2))
@@ -75,10 +61,6 @@ class L0Linear(Module):
         return self._reg_w()
 
     def count_expected_flops_and_l0(self):
-        """Measures the expected floating point operations (FLOPs) and the expected L0 norm"""
-        # dim_in multiplications and dim_in - 1 additions for each output neuron for the weights
-        # + the bias addition for each neuron
-        # total_flops = (2 * in_features - 1) * out_features + out_features
         ppos = torch.sum(1 - self.cdf_qz(0))
         expected_flops = (2 * ppos - 1) * self.out_features
         expected_l0 = ppos * self.out_features
@@ -88,18 +70,16 @@ class L0Linear(Module):
         return expected_flops.data[0], expected_l0.data[0]
 
     def get_eps(self, size):
-        """Uniform random numbers for the concrete distribution"""
         eps = self.floatTensor(size).uniform_(epsilon, 1-epsilon)
         eps = Variable(eps)
         return eps
 
     def sample_z(self, batch_size, sample=True):
-        """Sample the hard-concrete gates for training and use a deterministic value for testing"""
         if sample:
             eps = self.get_eps(self.floatTensor(batch_size, self.in_features))
             z = self.quantile_concrete(eps)
             return F.hardtanh(z, min_val=0, max_val=1)
-        else:  # mode
+        else:  
             pi = F.sigmoid(self.qz_loga).view(1, self.in_features).expand(batch_size, self.in_features)
             return F.hardtanh(pi * (limit_b - limit_a) + limit_a, min_val=0, max_val=1)
 
